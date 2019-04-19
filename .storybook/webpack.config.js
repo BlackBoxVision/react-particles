2720976703cf8885fb89067fa5fb6aab5b4ee163
jonatansalas <jonatansalas@live.com.ar>
2019-04-18 21:33:25 -0300

    docs: add support for prop-types table in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -12,6 +12,8 @@ module.exports = {
           plugins: [
             '@babel/plugin-proposal-object-rest-spread',
             '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-syntax-dynamic-import',
+            'babel-plugin-typescript-to-proptypes'
           ],
         },
       },
