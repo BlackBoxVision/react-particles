bca632291a55ff6832ed9807c6ab471f3df718a9
Matteo Bruni <ar3s@icloud.com>
2020-04-14 16:53:11 +0200

    perf: replaced particles.js with tsParticles
    replaced particles.js with tsParticles

--- src/components/Particles/Particles.tsx
+++ src/components/Particles/Particles.tsx
@@ -1,118 +1,95 @@
 import merge from 'lodash.merge';
 import React, { PureComponent } from 'react';
 import { getParams } from '../../utils';
+import { tsParticles } from 'tsparticles';
 
 interface ParticlesProps {
-  /**
-   * Determines wheter to render Particles
-   *
-   * @default "particles-js"
-   */
-  id?: string;
-  /**
-   * Determines the configuration for Particles JS
-   */
-  params: any;
-  /**
-   * Determines the styles for the Particles container
-   *
-   * @default {}
-   */
-  style?: any;
-  /**
-   * Determines the width for the Particles container
-   *
-   * @default "auto"
-   */
-  width?: string;
-  /**
-   * Determines the height for the Particles container
-   *
-   * @default "100vh"
-   */
-  height?: string;
-  /**
-   * Determines a custom className to apply to Particles container
-   */
-  className?: string;
-  /**
-   * Determines if should merge values from params with lib defaults
-   *
-   * @default true
-   */
-  withDefaults?: boolean;
-}
-
-interface CustomWindow extends Window {
-  particlesJS?: any;
-  pJSDom?: Array<any>;
+	/**
+	 * Determines wheter to render Particles
+	 *
+	 * @default "particles-js"
+	 */
+	id?: string;
+	/**
+	 * Determines the configuration for Particles JS
+	 */
+	params: any;
+	/**
+	 * Determines the styles for the Particles container
+	 *
+	 * @default {}
+	 */
+	style?: any;
+	/**
+	 * Determines the width for the Particles container
+	 *
+	 * @default "auto"
+	 */
+	width?: string;
+	/**
+	 * Determines the height for the Particles container
+	 *
+	 * @default "100vh"
+	 */
+	height?: string;
+	/**
+	 * Determines a custom className to apply to Particles container
+	 */
+	className?: string;
+	/**
+	 * Determines if should merge values from params with lib defaults
+	 *
+	 * @default true
+	 */
+	withDefaults?: boolean;
 }
 
 export class Particles extends PureComponent<ParticlesProps> {
-  static displayName = 'Particles';
+	static displayName = 'Particles';
 
-  static defaultProps = {
-    style: {},
-    width: 'auto',
-    height: '100vh',
-    id: 'particles-js',
-    withDefaults: true,
-  };
+	static defaultProps = {
+		style: {},
+		width: 'auto',
+		height: '100vh',
+		id: 'particles-js',
+		withDefaults: true,
+	};
 
-  componentDidMount() {
-    require('particles.js');
+	componentDidMount() {
+		tsParticles.load(this.props.id ?? "tsparticles", this.getParticles());
+	}
 
-    var w: CustomWindow = window;
+	componentDidUpdate(prevProps: ParticlesProps) {
+		tsParticles.load(this.props.id ?? "tsparticles", this.getParticles());
+	}
 
-    if ('particlesJS' in w) {
-      w.particlesJS(this.props.id, this.getParticles());
-    }
-  }
+	componentWillUnmount() {
+		tsParticles.dom().forEach((container) => container.destroy());
+	}
 
-  componentDidUpdate(prevProps: ParticlesProps) {
-    var w: CustomWindow = window;
+	render() {
+		const { className, style, width, height, id } = this.props;
 
-    if (
-      'particlesJS' in w &&
-      prevProps.id !== this.props.id &&
-      prevProps.params !== this.props.params
-    ) {
-      w.particlesJS(this.props.id, this.getParticles());
-    }
-  }
+		return (
+			<div
+				id={ id }
+				className={ className }
+				style={ {
+					...style,
+					width: width,
+					height: height,
+				} }
+			/>
+		);
+	}
 
-  componentWillUnmount() {
-    var w: CustomWindow = window;
-
-    if ('pJSDom' in w && w.pJSDom instanceof Array && w.pJSDom.length > 0) {
-      w.pJSDom.forEach(({ pJS }) => pJS && pJS.fn.vendors.destroypJS());
-      w.pJSDom = [];
-    }
-  }
-
-  render() {
-    const { className, style, width, height, id } = this.props;
-
-    return (
-      <div
-        id={id}
-        className={className}
-        style={{
-          ...style,
-          width: width,
-          height: height,
-        }}
-      />
-    );
-  }
-
-  getParticles = () => {
-    if (this.props.withDefaults) {
-      return merge({}, getParams(), this.props.params);
-    } else {
-      return this.props.params;
-    }
-  };
+	getParticles = () => {
+		if (this.props.withDefaults) {
+			return merge({}, getParams(), this.props.params);
+		} else {
+			return this.props.params;
+		}
+	};
 }
 
 export default Particles;
